e0efe088f29739a371116a9d78c4a9ff55d5a8f3
RichardPMachado <richardmachado2211@gmail.com>
2023-02-17 20:18:40 -0300

    fix[requistio 8]; corrigindo SimpleFighter.test estava sendo importado do local errado

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,10 +4,6 @@ import SimpleFighter from './SimpleFighter';
 export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
-  special?(enemy: Fighter): void;
+  special?(enemy: Fighter | SimpleFighter): void;
   levelUp(): void;
 }
-
-//  name: deve ser recebido como parâmetro no 
-//  construtor e deve ser usado para dar nome 
-//  à sua personagem.
